1d6bd358e62089664954dd3849973ab855968928
ilyukevich <9009901@gmail.com>
2021-08-22 20:25:51 +0300

    UPD serializers for imags, category

--- apps/imgapp/api/serializers.py
+++ apps/imgapp/api/serializers.py
@@ -7,13 +7,26 @@ class CategorySerializer(serializers.ModelSerializer):
     """Serializer for Category"""
 
     class Meta:
-        fields = '__all__'
+        #fields = '__all__'
+        fields = ['id', 'name', 'name', 'url']
         model = Category
+        extra_kwargs = {
+            'url': {
+                'view_name': 'api:category-detail'
+            },
+        }
 
 
 class ImageSerializer(serializers.ModelSerializer):
     """Serializer for Image"""
 
     class Meta:
-        fields = '__all__'
+        #fields = '__all__'
+        fields = ['id', 'author', 'category', 'name', 'slug',
+                  'image', 'description', 'available', 'created', 'updated', 'url']
         model = Image
+        extra_kwargs = {
+            'url': {
+                'view_name': 'api:images-detail'
+            },
+        }
